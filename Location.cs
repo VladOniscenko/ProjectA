52c2e2bdf0e51cd28b66d927ddcfd97d21bf0482
younis1105925 <1105925@hr.nl>
2025-02-18 22:36:42 +0100

    Update Location.cs

--- Location.cs
+++ Location.cs
@@ -3,12 +3,12 @@ public class Location
         public int ID;
         public string Name;
         public string Description;
-        public Quest? QuestAvailableHere;
-        public Monster? MonsterLivingHere;
-        public Location? LocationToNorth = null; 
-        public Location? LocationToEast = null;
-        public Location? LocationToSouth = null; 
-        public Location? LocationToWest = null; 
+        public Quest QuestAvailableHere;
+        public Monster MonsterLivingHere;
+        public Location? LocationToNorth; 
+        public Location? LocationToEast;
+        public Location? LocationToSouth; 
+        public Location? LocationToWest; 
 
 
         public Location(int ID, string Name, string Description, Quest QuestAvailableHere, Monster MonsterLivingHere)
