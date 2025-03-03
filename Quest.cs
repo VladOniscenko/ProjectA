9af614a1d91774e9260ab69074aef1a639814197
Oni <vladoniscenko@outlook.com>
2025-03-03 13:36:20 +0100

    remove monster and location from quest

--- Quest.cs
+++ Quest.cs
@@ -15,21 +15,17 @@ public class Quest
     public string QuestName;
     public string QuestDescription;
     public string QuestGiver;
-    public Monster MonsterToKill;
     public int AmountOfMonstersToKill;
-    public Location LocationOfQuest;
     public string Reward;
     public bool IsCompleted;
 
     // Volledige constructor
-    public Quest(string name, string description, string giver, Monster monster, int amount, Location location, string reward)
+    public Quest(string name, string description, string giver, int amount, string reward)
     {
         QuestName = name;
         QuestDescription = description;
         QuestGiver = giver;
-        MonsterToKill = monster;
         AmountOfMonstersToKill = amount;
-        LocationOfQuest = location;
         Reward = reward;
         IsCompleted = false; // Default op false
     }
