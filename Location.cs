1e1c5f8d07f5e185e742cc0a6b9e4b0a26533bf6
Oni <vladoniscenko@outlook.com>
2025-03-03 20:34:55 +0100

    new field

--- Location.cs
+++ Location.cs
@@ -9,6 +9,7 @@ public class Location
         public Location? LocationToEast;
         public Location? LocationToSouth; 
         public Location? LocationToWest;
+        public Quest? FightAvailableHere;
         public string? QuestGiver;
 
         public Location(int id, string name, string description, Quest? questAvailableHere = null, Monster? monsterLivingHere = null, string? questGiver = null)
@@ -30,9 +31,7 @@ public class Location
             bool south = LocationToSouth is not null;
             bool west = LocationToWest is not null;
             string locationName = Name;
-            string locationDescription = Description;
 
-            Console.WriteLine(locationDescription);
             Console.WriteLine($"You are at: {locationName} From here you can go:");
             if (north)
             {
