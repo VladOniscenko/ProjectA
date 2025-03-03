5dd2165d152557317e16c8688ab6d0961821d83e
DimitriK-04 <dimitrikorenhof@gmail.com>
2025-03-03 22:22:38 +0100

    added a minimap that makes current location red

--- Location.cs
+++ Location.cs
@@ -31,6 +31,80 @@ public class Location
             string locationName = Name;
             string locationDescription = Description;
 
+
+            char[,] miniMap = {
+            { ' ', ' ', 'P', ' ', ' ', ' ' },
+            { ' ', ' ', 'A', ' ', ' ', ' ' },
+            { 'V', 'F', 'T', 'G', 'B', 'S' },
+            { ' ', ' ', 'H', ' ', ' ', ' ' }
+            };
+            int currentX = 2;
+            int currentY = 3;
+            switch(ID)
+            {
+                case 1:
+                currentY = 3;
+                break;
+
+                case 2:
+                currentY = 2;
+                break;
+
+                case 3:
+                currentX = 3;
+                currentY = 2;
+                break;
+
+                case 4:
+                currentY = 1;
+                break;
+
+                case 5:
+                currentY = 0;
+                break;
+
+                case 6:
+                currentX = 1;
+                currentY = 2;
+                break;
+
+                case 7:
+                currentX = 0;
+                currentY = 2;
+                break;
+
+                case 8:
+                currentX = 4;
+                currentY = 2;
+                break;
+
+                case 9:
+                currentX = 5;
+                currentY = 2;
+                break;
+
+            }
+        
+            // List<string> minimap = new List<string>(locations);
+
+            for (int i = 0; i < miniMap.GetLength(0); i++)
+            {
+                for (int j = 0;j < miniMap.GetLength(1); j++)
+                {
+                    if (j == currentX && i == currentY)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(miniMap[i, j]); 
+                        Console.ForegroundColor = ConsoleColor.Gray; 
+                    }
+                    else
+                    {
+                        Console.Write(miniMap[i, j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+
             Console.WriteLine(locationDescription);
             Console.WriteLine($"You are at: {locationName} From here you can go:");
             if (north)
